ffeaf8e5531e20459f522aefbcc00f48aa648f5a
finnyjules <151522583+finnyjules@users.noreply.github.com>
2026-06-14 21:00:48 -0700

    feat(spacetype): register Cylinder, Field, Coil effects in the picker

--- frontend/app/lib/spacetype/effects/index.ts
+++ frontend/app/lib/spacetype/effects/index.ts
@@ -1,11 +1,17 @@
 import type { SpaceTypeEffect } from '../effect'
 import { ribbonEffect } from './ribbon'
 import { stripesEffect } from './stripes'
+import { cylinderEffect } from './cylinder'
+import { fieldEffect } from './field'
+import { coilEffect } from './coil'
 
 /** All registered Space Type effects, in picker order. Add new effect modules here. */
 export const SPACE_TYPE_EFFECTS: SpaceTypeEffect[] = [
   ribbonEffect,
   stripesEffect,
+  cylinderEffect,
+  fieldEffect,
+  coilEffect,
 ]
 
 export function getEffect(id: string): SpaceTypeEffect {
